5a8ec728647d9dc892d9347e0ee1a5d5475c1cc6
Unknown <g1727018@st.t-kougei.ac.jp>
2018-05-02 17:27:28 +0900

    最初のコミット

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 3; i >= 0; i--)
+        {
+            Debug.Log(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
